432570547a4c448457292c8e4b47fad43ed4e322
Krasnov Sergey <sergey.krasnov@edgecenter.ru>
2026-03-06 17:28:58 +0300

    add pointer

--- edgecenter/errors_test.go
+++ edgecenter/errors_test.go
@@ -11,27 +11,27 @@ func TestErrorResponse_Error(t *testing.T) {
 	errorsFilled := json.RawMessage(`{"code":"validation_error"}`)
 	tests := []struct {
 		name     string
-		resp     ErrorResponse
+		resp     *ErrorResponse
 		expected string
 	}{
 		{
 			name:     "message set returns message",
-			resp:     ErrorResponse{Message: "something went wrong"},
+			resp:     &ErrorResponse{Message: "something went wrong"},
 			expected: "something went wrong",
 		},
 		{
 			name:     "message empty errors set returns json",
-			resp:     ErrorResponse{Errors: &errorsFilled},
+			resp:     &ErrorResponse{Errors: &errorsFilled},
 			expected: "{\n\t\"code\": \"validation_error\"\n}",
 		},
 		{
 			name:     "message empty errors nil returns null",
-			resp:     ErrorResponse{},
+			resp:     &ErrorResponse{},
 			expected: "null",
 		},
 		{
 			name:     "both set message has priority",
-			resp:     ErrorResponse{Message: "priority message", Errors: &errorsFilled},
+			resp:     &ErrorResponse{Message: "priority message", Errors: &errorsFilled},
 			expected: "priority message",
 		},
 	}
